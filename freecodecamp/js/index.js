e19396cf93688cb74eb6be7a5300107d6e6b4853
shanthikaruna <shanthi.karuna01@gmail.com>
2023-12-01 12:28:37 -0800

    add all files

---
+++ freecodecamp/js/index.js
@@ -0,0 +1,9 @@
+/*Collection of Freecodecamp JS <exercises></exercises>
+
+//JS strings are immutable
+let myStr = "Jello World";
+
+// Only change code below this line
+//myStr[0] = "H" --> This cannot be done
+// Only change code above this line
+myStr = "Hello World";
